11289cb4e5996d6cb574d201436a06d905142d9d
kamaaki <akihiro16910@gmail.com>
2024-10-22 14:31:46 +0900

    feat: setup sass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        css: {
+            preprocessorOptions: `@import "src/styles/_responsive.scss", "src/styles/_color.scss", "src/styles/_button1.scss";`
+        }
+    }
+});
