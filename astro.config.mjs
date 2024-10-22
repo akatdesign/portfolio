acece95da79ea3323e9b752834b4f0b2f1d8ee29
kamaaki <akihiro16910@gmail.com>
2024-10-22 15:08:21 +0900

    refactor: scss and css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        css: {
-            preprocessorOptions: `@import "src/styles/_responsive.scss", "src/styles/_color.scss", "src/styles/_button1.scss";`
+        scss: {
+            additionalData: `@import "src/styles/_responsive.scss", "src/styles/_color.scss", "src/styles/_button1.scss";`
         }
     }
 });
