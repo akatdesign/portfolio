4fb46f7d26d152274db102402ba9b3eb7805c391
akatdesign <akihiro16910@gmail.com>
2025-05-29 04:12:22 +0900

    refactor: lenis.js commrnt

--- src/scripts/lenis.js
+++ src/scripts/lenis.js
@@ -1,10 +1,4 @@
 import Lenis from "lenis";
-/**
- * イージング関数
- * @see https://easings.net/ja
- * @param {number} x アニメーションの進行度（正規化された0から1までの値）
- * @return {number} イージングを適用した後の進行度（正規化された0から1までの値）
- */
 
 const easePutQuart = (x) => {
   return 1 - Math.pow(1 - x, 4);
